b342247c73afe63b51e1e132748e1071050a013b
Chris <chris@antrapol.com>
2024-06-27 01:03:55 +0800

    fixes

--- lib/ruby_ex_port.rb
+++ lib/ruby_ex_port.rb
@@ -2,6 +2,8 @@
 
 require_relative "ruby_ex_port/version"
 
+require_relative "ruby_ex_port/port_server"
+
 module RubyExPort
   class Error < StandardError; end
 end
--- lib/ruby_ex_port/port_server.rb
+++ lib/ruby_ex_port/port_server.rb
@@ -19,15 +19,29 @@ module RubyExPort
       context = binding
       while (cmd = read(inp))
         puts "Ruby Command: #{cmd[0]}\r"
-        if cmd[0] == "invoke"
+        puts "Ruby Command Options: #{convert_map(cmd[4].to_hash)}\r"
+        opts = convert_map(cmd[4].to_hash)
+        if cmd[0].to_s == "invoke"
           # puts "command : #{cmd[1]}.send(:#{cmd[2]}, *#{cmd[3].to_a})}"
-          res = if not_empty?(cmd[1])
-            eval("#{cmd[1]}.send(:#{cmd[2]}, *#{cmd[3].to_a})", context, __FILE__, __LINE__)
-          else
-            eval("send(:#{cmd[2]}, *#{cmd[3].to_a})", context, __FILE__, __LINE__)
+          begin
+            res = if not_empty?(cmd[1])
+              eval("#{cmd[1]}.send(:#{cmd[2]}, *#{cmd[3].to_a})", context, __FILE__, __LINE__)
+            else
+              eval("send(:#{cmd[2]}, *#{cmd[3].to_a})", context, __FILE__, __LINE__)
+            end
+            puts "Eval result: #{res.inspect}\n\r"
+
+            if opts.keys.include?(:as_var)
+              instance_variable_set("@#{opts[:as_var]}", res)
+              write(oup, 'ok')
+            else
+              write(oup, 'ok', res)
+            end
+
+            #write(oup, 'ok', res)
+          rescue StandardError => exception
+            write(oup, 'error', exception.message)  
           end
-          puts "Eval result: #{res.inspect}\n\r"
-          write(oup)
         end
       end
 
@@ -45,6 +59,26 @@ module RubyExPort
       [STDIN, STDOUT]
     end
 
+    def convert_map(map)
+      map.collect do |k,v|
+        case k
+        when Erlang::Atom
+          kk = k.to_s.to_sym
+        else
+        kk = k.to_s
+        end
+
+        case v
+        when Erlang::Atom
+          vv = v.to_s.to_sym
+        else
+          vv = v.to_s
+        end
+
+        [kk, vv]
+      end.to_h
+    end
+
     def read(inp)
       encoded_length = inp.read(4)
       return nil unless encoded_length
@@ -55,13 +89,17 @@ module RubyExPort
       cmd
     end
 
-    def write(oup, value)
-      # response = Erlang.term_to_binary(Erlang::Tuple[@request_id, value])
-      response = Erlang.term_to_binary(Erlang::Tuple[Erlang::Atom['ok'], value])
+    def write(oup, prefix, value = nil)
+      #if value.nil?
+      #  response = Erlang.term_to_binary(Erlang::Atom[prefix])
+      #else
+        # response = Erlang.term_to_binary(Erlang::Tuple[@request_id, value])
+        response = Erlang.term_to_binary(Erlang::Tuple[Erlang::Atom[prefix], value])
+      #end
+
       oup.write([response.bytesize].pack('N'))
       oup.write(response)
       true
     end
-
   end
 end
